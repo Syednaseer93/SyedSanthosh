e0b910a206aca5f533aa10a06ed91d28ee622f2e
Santhu9986 <114332944+Santhu9986@users.noreply.github.com>
2023-04-19 21:27:55 +0530

    Update Naseer.java

--- src/main/java/Naseer.java
+++ src/main/java/Naseer.java
@@ -4,5 +4,6 @@ public class Naseer {
 public static void main(String[] args) {
 	System.out.println("--- from master--");
 	System.out.println("----from santhosh---");
+	System.out.println("----from santhosh---");
 }
 }
